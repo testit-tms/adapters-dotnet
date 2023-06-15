83be3a26c632095d9783744d7e6ed0d89618fd1a
Dmitry.Gridnev <dmitry.gridnev@testit.software>
2023-06-15 11:38:03 +0200

    Small refactor

--- Tms.Adapter.SpecFlowPlugin/TmsBindingInvoker.cs
+++ Tms.Adapter.SpecFlowPlugin/TmsBindingInvoker.cs
@@ -23,162 +23,158 @@ namespace Tms.Adapter.SpecFlowPlugin
             ITestTracer testTracer,
             out TimeSpan duration)
         {
-            if (binding is HookBinding hook)
+            if (binding is not HookBinding hook)
+                return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+            
+            var featureContainerId = TmsHelper.GetFeatureContainerId(contextManager.FeatureContext?.FeatureInfo);
+
+            switch (hook.HookType)
             {
-                var featureContainerId = TmsHelper.GetFeatureContainerId(contextManager.FeatureContext?.FeatureInfo);
-
-                switch (hook.HookType)
-                {
-                    case HookType.BeforeFeature:
-                        if (hook.HookOrder == int.MinValue)
-                        {
-                            var featureContainer = new ClassContainer
-                            {
-                                Id = TmsHelper.GetFeatureContainerId(contextManager.FeatureContext?.FeatureInfo)
-                            };
-                            Adapter.StartTestContainer(featureContainer);
-
-                            contextManager.FeatureContext.Set(new HashSet<ClassContainer>());
-                            contextManager.FeatureContext.Set(new HashSet<TestContainer>());
-
-                            return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
-                        }
-
-                        try
-                        {
-                            StartFixture(hook, featureContainerId);
-                            var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
-                                out duration);
-                            Adapter.StopFixture(x => x.Status = Status.Passed);
-                            return result;
-                        }
-                        catch (Exception ex)
-                        {
-                            Adapter.StopFixture(x => x.Status = Status.Failed);
-                                
-                            var scenarioContainer =
-                                TmsHelper.StartTestContainer(contextManager.FeatureContext, null);
-                                
-                            var scenario = TmsHelper.StartTestCase(scenarioContainer.Id,
-                                contextManager.FeatureContext, null);
-                                
-                            Adapter
-                                .StopTestCase(x =>
-                                {
-                                    x.Status = Status.Failed;
-                                    x.Message = ex.Message;
-                                    x.Trace = ex.StackTrace;
-                                })
-                                .StopTestContainer(scenarioContainer.Id)
-                                .WriteTestCase(scenario.Id, scenarioContainer.Id);
-
-                            throw;
-                        }
-
-                    case HookType.BeforeStep:
-                    case HookType.AfterStep:
+                case HookType.BeforeFeature:
+                    if (hook.HookOrder == int.MinValue)
                     {
-                        var scenario = TmsHelper.GetCurrentTestCase(contextManager.ScenarioContext);
+                        var featureContainer = new ClassContainer
+                        {
+                            Id = TmsHelper.GetFeatureContainerId(contextManager.FeatureContext?.FeatureInfo)
+                        };
+                        Adapter.StartTestContainer(featureContainer);
 
-                        try
-                        {
-                            return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
-                        }
-                        catch (Exception ex)
-                        {
-                            Adapter
-                                .UpdateTestCase(scenario.Id,
-                                    x =>
-                                    {
-                                        x.Status = Status.Failed;
-                                        x.Message = ex.Message;
-                                        x.Trace = ex.StackTrace;
-                                    });
-                            throw;
-                        }
+                        contextManager.FeatureContext.Set(new HashSet<ClassContainer>());
+                        contextManager.FeatureContext.Set(new HashSet<TestContainer>());
+
+                        return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
                     }
 
-                    case HookType.BeforeScenario:
-                    case HookType.AfterScenario:
-                        if (hook.HookOrder == int.MinValue || hook.HookOrder == int.MaxValue)
-                        {
-                            return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
-                        }
-
+                    try
                     {
-                        var scenarioContainer = TmsHelper.GetCurrentTestContainer(contextManager.ScenarioContext);
+                        StartFixture(hook, featureContainerId);
+                        var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
+                            out duration);
+                        Adapter.StopFixture(x => x.Status = Status.Passed);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        Adapter.StopFixture(x => x.Status = Status.Failed);
 
-                        try
-                        {
-                            StartFixture(hook, scenarioContainer.Id);
-                            var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
-                                out duration);
-                            Adapter.StopFixture(x => x.Status = Status.Passed);
-                            return result;
-                        }
-                        catch (Exception ex)
-                        {
-                            Adapter.StopFixture(x => x.Status = Status.Failed);
-                            
-                            var scenario = TmsHelper.GetCurrentTestCase(contextManager.ScenarioContext) ??
-                                           TmsHelper.StartTestCase(scenarioContainer.Id,
-                                               contextManager.FeatureContext, contextManager.ScenarioContext);
+                        var scenarioContainer =
+                            TmsHelper.StartTestContainer(contextManager.FeatureContext, null);
 
-                            Adapter.UpdateTestCase(scenario.Id,
+                        var scenario = TmsHelper.StartTestCase(scenarioContainer.Id,
+                            contextManager.FeatureContext, null);
+
+                        Adapter
+                            .StopTestCase(x =>
+                            {
+                                x.Status = Status.Failed;
+                                x.Message = ex.Message;
+                                x.Trace = ex.StackTrace;
+                            })
+                            .StopTestContainer(scenarioContainer.Id)
+                            .WriteTestCase(scenario.Id, scenarioContainer.Id);
+
+                        throw;
+                    }
+
+                case HookType.BeforeStep:
+                case HookType.AfterStep:
+                {
+                    var scenario = TmsHelper.GetCurrentTestCase(contextManager.ScenarioContext);
+
+                    try
+                    {
+                        return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+                    }
+                    catch (Exception ex)
+                    {
+                        Adapter
+                            .UpdateTestCase(scenario.Id,
                                 x =>
                                 {
                                     x.Status = Status.Failed;
                                     x.Message = ex.Message;
                                     x.Trace = ex.StackTrace;
                                 });
-                            throw;
-                        }
+                        throw;
+                    }
+                }
+
+                case HookType.BeforeScenario:
+                case HookType.AfterScenario:
+                    if (hook.HookOrder == int.MinValue || hook.HookOrder == int.MaxValue)
+                    {
+                        return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
                     }
 
-                    case HookType.AfterFeature:
-                        if (hook.HookOrder == int.MaxValue)
-                        {
-                            WriteScenarios(contextManager);
+                {
+                    var scenarioContainer = TmsHelper.GetCurrentTestContainer(contextManager.ScenarioContext);
 
-                            return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
-                        }
+                    try
+                    {
+                        StartFixture(hook, scenarioContainer.Id);
+                        var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
+                            out duration);
+                        Adapter.StopFixture(x => x.Status = Status.Passed);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        Adapter.StopFixture(x => x.Status = Status.Failed);
 
-                        try
-                        {
-                            StartFixture(hook, featureContainerId);
-                            var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
-                                out duration);
-                            Adapter.StopFixture(x => x.Status = Status.Passed);
-                            return result;
-                        }
-                        catch (Exception ex)
-                        {
-                            var scenario = contextManager.FeatureContext.Get<HashSet<TestContainer>>().Last();
-                            Adapter
-                                .StopFixture(x => x.Status = Status.Failed)
-                                .UpdateTestCase(scenario.Id,
-                                    x =>
-                                    {
-                                        x.Status = Status.Failed;
-                                        x.Message = ex.Message;
-                                        x.Trace = ex.StackTrace;
-                                    });
+                        var scenario = TmsHelper.GetCurrentTestCase(contextManager.ScenarioContext);
 
-                            WriteScenarios(contextManager);
-
-                            throw;
-                        }
-
-                    case HookType.BeforeScenarioBlock:
-                    case HookType.AfterScenarioBlock:
-                    case HookType.BeforeTestRun:
-                    case HookType.AfterTestRun:
-                    default:
-                        return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+                        Adapter.UpdateTestCase(scenario.Id,
+                            x =>
+                            {
+                                x.Status = Status.Failed;
+                                x.Message = ex.Message;
+                                x.Trace = ex.StackTrace;
+                            });
+                        throw;
+                    }
                 }
-            }
 
-            return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+                case HookType.AfterFeature:
+                    if (hook.HookOrder == int.MaxValue)
+                    {
+                        WriteScenarios(contextManager);
+
+                        return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+                    }
+
+                    try
+                    {
+                        StartFixture(hook, featureContainerId);
+                        var result = base.InvokeBinding(binding, contextManager, arguments, testTracer,
+                            out duration);
+                        Adapter.StopFixture(x => x.Status = Status.Passed);
+                        return result;
+                    }
+                    catch (Exception ex)
+                    {
+                        var scenario = contextManager.FeatureContext.Get<HashSet<TestContainer>>().Last();
+                        Adapter
+                            .StopFixture(x => x.Status = Status.Failed)
+                            .UpdateTestCase(scenario.Id,
+                                x =>
+                                {
+                                    x.Status = Status.Failed;
+                                    x.Message = ex.Message;
+                                    x.Trace = ex.StackTrace;
+                                });
+
+                        WriteScenarios(contextManager);
+
+                        throw;
+                    }
+
+                case HookType.BeforeScenarioBlock:
+                case HookType.AfterScenarioBlock:
+                case HookType.BeforeTestRun:
+                case HookType.AfterTestRun:
+                default:
+                    return base.InvokeBinding(binding, contextManager, arguments, testTracer, out duration);
+            }
         }
 
         private static void StartFixture(HookBinding hook, string containerId)
