a49ac0c49eba33ec05af1155727a4f81e7ff49e5
Petr Komissarov <petr.komissarov@testit.software>
2024-05-24 12:27:33 +0300

    Add new unit tests

--- Tms.Adapter.CoreTests/Configurator/ConfiguratorTests.cs
+++ Tms.Adapter.CoreTests/Configurator/ConfiguratorTests.cs
@@ -5,16 +5,19 @@ namespace Tms.Adapter.CoreTests.Configurator;
 [TestClass]
 public class ConfiguratorTests
 {
-    [TestMethod]
-    public void ConvertAutoTestDtoToPostModel()
+    [TestInitialize]
+    public void InitializeTest()
     {
-        // Arrange
         Environment.SetEnvironmentVariable("TMS_URL", "https://example.com");
         Environment.SetEnvironmentVariable("TMS_PRIVATE_TOKEN", "token");
         Environment.SetEnvironmentVariable("TMS_PROJECT_ID", Guid.NewGuid().ToString());
         Environment.SetEnvironmentVariable("TMS_CONFIGURATION_ID", Guid.NewGuid().ToString());
-        Environment.SetEnvironmentVariable("TMS_TEST_RUN_ID", Guid.NewGuid().ToString());
-        
+        Environment.SetEnvironmentVariable("TMS_TEST_RUN_ID", Guid.NewGuid().ToString()); 
+    }
+    
+    [TestMethod]
+    public void GetConfig()
+    {
         // Act
         var actual = Core.Configurator.Configurator.GetConfig();
 
---
+++ Tms.Adapter.CoreTests/Service/AdapterTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Tms.Adapter.CoreTests.Service;
+
+[TestClass]
+public class AdapterTests
+{
+    [TestInitialize]
+    public void InitializeTest()
+    {
+        Environment.SetEnvironmentVariable("TMS_URL", "https://example.com");
+        Environment.SetEnvironmentVariable("TMS_PRIVATE_TOKEN", "token");
+        Environment.SetEnvironmentVariable("TMS_PROJECT_ID", Guid.NewGuid().ToString());
+        Environment.SetEnvironmentVariable("TMS_CONFIGURATION_ID", Guid.NewGuid().ToString());
+        Environment.SetEnvironmentVariable("TMS_TEST_RUN_ID", Guid.NewGuid().ToString()); 
+    }
+
+    [TestMethod]
+    public void AddMessage()
+    {
+        // Arrange
+        const string message = "test";
+        
+        // Act & Assert
+        Core.Service.Adapter.AddMessage(message);
+    }
+    
+    [TestMethod]
+    public void AddAttachments()
+    {
+        var filename = Path.GetRandomFileName();
+        
+        try
+        {
+            // Arrange
+            File.Create(filename).Dispose();
+        
+            // Act & Assert
+            Assert.ThrowsException<AggregateException>(() => Core.Service.Adapter.AddAttachments(filename));
+        }
+        finally
+        {
+            File.Delete(filename);
+        }
+    }
+
+    [TestMethod]
+    public void AddLinks()
+    {
+        // Arrange
+        const string url = "https://example.com";
+
+        // Act & Assert
+        Core.Service.Adapter.AddLinks(url);
+    }
+}
